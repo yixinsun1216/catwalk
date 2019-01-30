e3e6f420a5f517600c10316d1170279fa81c9b35
Michael Cahana <cahanamichael@gmail.com>
2019-01-30 15:33:17 -0600

    adding buggy test code so sunny can see

---
+++ R/read_latex.R
@@ -0,0 +1,82 @@
+library(tidyverse)
+library(knitr)
+library(readr)
+
+
+#===========
+# read latex
+#===========
+
+#===========
+# do stuff
+#===========
+
+read_latex <- function(latex_file, output = 'coef') {
+
+  latex <- readLines(latex_file) %>% .[. != ""]
+
+  # IDEA: split up the latex output into the 4 main components:
+  # 1. Header
+  # 2. Coefficients/se
+  # 3. Extra lines
+  # 4. summary stats
+  # split into sections by "midrule"
+
+  split_vec <- function(vec, sep = 0) {
+      is.sep <- vec == sep
+      split(vec[!is.sep], cumsum(is.sep)[!is.sep])
+  }
+
+  latex_split <- split_vec(latex, sep = "\\midrule")
+
+
+  header <- latex_split[[1]]
+  coef <- latex_split[[2]]
+
+  if(length(latex_split) == 3){
+    stats <- latex_split[[3]]
+  } else if(length(latex_split) == 4){
+    stats <- latex_split[[4]]
+    extra <- latex_split[[3]] %>% 
+      str_split("&") %>%
+      map_df(function(x) as_tibble(t(x))) %>%
+      rename(extra_name = 1) %>%
+      filter(!str_detect(extra_name, "\\\\")) %>%
+      mutate_all(trimws) %>%
+      mutate_all(funs(str_replace_all(., "\\\\", "")))
+
+  }
+
+  # stats and extra can be made into dataframes
+  stats <- 
+    stats %>%
+    str_split("&") %>%
+    map_df(function(x) as_tibble(t(x))) %>%
+    rename(stats_name = 1) %>%
+    filter(!str_detect(stats_name, "\\\\")) %>%
+    mutate_all(trimws) %>%
+    mutate_all(funs(str_replace_all(., "\\\\", "")))
+
+  # coefficients
+  coef <-
+    coef %>%
+    str_split("&") %>%
+    map_df(function(x) as_tibble(t(x))) %>%
+    rename(est_name = 1) %>%
+    mutate(est_name = str_extract(est_name, "(\\s.*?)(\\})"), 
+      est_name = str_replace(est_name, "\\}", "")) %>%
+    mutate_all(trimws) %>%
+    mutate_all(funs(str_replace_all(., "\\\\", ""))) %>%
+    mutate(type = if_else(is.na(est_name), "coef", "se")) %>%
+    fill(est_name, .direction = "up") 
+
+  if (output=='coef') {
+    return (coef)
+  } else if (output=='stats') {
+    return (stats)
+  } else if (output=='extra') {
+    return (extra)
+  } else {
+    stop("invalid output choice (choose either 'coef', 'stats', or 'extra')")
+  }
+}
--- R/test.R
+++ R/test.R
@@ -1,20 +1,192 @@
+library(tidyverse)
+library(testthat)
+require(stats)
+library(modelr)
 library(lfe)
 
-## create covariates
-x1 <- rnorm(1000)
-x2 <- rnorm(length(x1))
+#===========
+# read in
+#===========
 
-## fixed effects
-fe <- factor(sample(20, length(x1), replace=TRUE))
+source("regtable.R"))
+source("read_latex.R"))
 
-## effects for fe
-fe_effs <- rnorm(nlevels(fe))
+#===========
+# function
+#===========
 
-## creating left hand side y
-u <- rnorm(length(x1))
-y <- 2 * x1 + x2 + fe_effs[fe] + u
+standard_equivalence_tests <- function(model, latex_output, dual = FALSE) {
 
-m <- felm(y ~ x1 + x2 | fe)
+	model = model_list[[1]]
 
-regtable(m, "x1", mnames = "(1)", stats = c("N", "r2"),
-         stats_names = c("N", "$R^2$"), sig_stars = TRUE)
+	latex_file <- file.path(root, 'tests', 'latex', 'temp_regression.tex')
+
+	writeLines(latex_output, latex_file)
+
+	test_that("testing coefficient equivalence", {
+		count = 0
+		while (count<length(model_list)) {
+			count = count + 1
+			print(count)
+			latex_coef <- read_latex(latex_file, output = 'coef') %>% 
+				filter(type=='coef') %>% 
+				select(-c(est_name, type)) %>% 
+				pull(-count) %>% 
+				as.double()
+			model_coef <-  model_list[[-count]] %>% 
+				summary() %>% 
+				coef() %>% 
+				.[-1,1] %>% 
+				as.double() %>% 
+				round(3)
+			if (length(model_coef) < length(latex_coef)) {
+				model_coef <-  model_list[[-count]] %>% 
+					summary() %>% 
+					coef() %>% 
+					.[,1] %>% 
+					as.double() %>% 
+					round(3)
+			}
+			print(latex_coef)
+			print(model_coef) 
+			expect_equal(model_coef, latex_coef)
+		}
+	})
+
+	test_that("testing standard error equivalence", {
+		count = 0
+		while (count<length(model_list)) {
+			count = count + 1
+			latex_se <- read_latex(latex_file, output = 'coef') %>% 
+				filter(type=='se') %>% 
+				select(-c(est_name, type)) %>% 
+				pull(-count) %>% 
+				str_replace_all("\\)", '') %>%
+				str_replace_all("\\(", '') %>%
+				as.double()
+			model_se <-  model_list[[-count]] %>% 
+				summary() %>% 
+				coef() %>% 
+				.[-1,2] %>% 
+				as.double() %>% 
+				round(3)
+			if (length(model_se) < length(latex_se)) {
+				model_se <-  model_list[[-count]] %>% 
+					summary() %>% 
+					coef() %>% 
+					.[,2] %>% 
+					as.double() %>% 
+					round(3)
+			}
+			expect_equal(model_se, latex_se)
+		}
+	})
+
+	test_that("testing projected R^2 equivalence", {
+		count = 0
+		while (count<length(model_list)) {
+			count = count + 1
+			latex_projected_R2 <- read_latex(latex_file, output = 'stats') %>% 
+				filter(stats_name=='Proj. $R^2$') %>% 
+				select(-stats_name) %>% 
+				pull(-count) %>% 
+				as.double()
+			model_projected_R2 <- model_list[[-count]] %>% 
+				summary() %>% 
+				.$adj.r.squared %>% 
+				round(3)
+			expect_equal(model_projected_R2, latex_projected_R2)
+		}
+	})
+
+	test_that("testing N equivalence", {
+		latex_N <- read_latex(latex_file, output = 'stats') %>% 
+			filter(stats_name=='N') %>% 
+			select(-stats_name) %>% 
+			pull() %>% 
+			as.double()
+		model_N <- model %>% 
+			nobs()
+		expect_equal(model_N, latex_N)
+	})
+}
+
+test_model <- function(model_list, test_statement, est, est_names = NULL, 
+	extra_rows = NULL) {
+
+	test_that(test_statement, {
+
+	latex_output <-
+			model_list %>%
+			regtable(., est = est,  
+				est_names = est_names, 
+				output_format = "latex", 
+				extra_rows = extra_rows)
+
+	standard_equivalence_tests(model, latex_output)
+	
+	})
+}
+
+#===========
+# examples
+#===========
+
+lm_fits <- mtcars %>% fit_with(lm, formulas(~disp,
+		one = ~drat, 
+		two = ~drat + cyl,
+		three = ~drat * cyl,
+		four = add_predictors(three, ~am), 
+		five = add_predictors(three, ~am, ~vs)
+		))
+
+test_model(list(lm_fits$one), "testing single lm model, one independent variable", 
+	est = 'drat')
+
+test_model(list(lm_fits$two), "testing single lm model, two independent variables", 
+	est = c('drat', 'cyl'))
+
+test_model(list(lm_fits$three), "testing single lm model, three independent variables", 
+	est = c('drat', 'cyl', 'drat:cyl'))
+
+test_model(list(lm_fits$four), "testing single lm model, four independent variables", 
+	est = c('drat', 'cyl', 'am', 'drat:cyl'))
+
+test_model(list(lm_fits$five), "testing single lm model, five independent variables", 
+	est = c('drat', 'cyl', 'am', 'vs', 'drat:cyl'))
+
+felm_fits <- mtcars %>% fit_with(felm, formulas(~disp,
+		one = ~drat, 
+		two = ~drat + cyl,
+		three = ~drat * cyl,
+		four = add_predictors(three, ~am), 
+		five = add_predictors(three, ~am, ~vs)
+		))
+
+test_model(list(felm_fits$one), "testing single felm model, one independent variable", 
+	est = 'drat')
+
+test_model(list(felm_fits$two), "testing single felm model, two independent variables", 
+	est = c('drat', 'cyl'))
+
+test_model(list(felm_fits$three), "testing single felm model, three independent variables", 
+	est = c('drat', 'cyl', 'drat:cyl'))
+
+test_model(list(felm_fits$four), "testing single felm model, four independent variables", 
+	est = c('drat', 'cyl', 'am', 'drat:cyl'))
+
+test_model(list(felm_fits$five), "testing single felm model, five independent variables", 
+	est = c('drat', 'cyl', 'am', 'vs', 'drat:cyl'))
+
+
+model1 <- mtcars %>% 
+	felm(disp ~ drat + cyl , data = .)
+
+model2 <- mtcars %>% 
+	rownames_to_column('company') %>% 
+	mutate(company = word(company)) %>% 
+	felm(disp ~ drat + cyl | company, data = .)
+
+test_model(list(model1, model2), "testing dual felm model, two independent variables", 
+	est = c('drat', 'cyl'), extra_rows = list("Company" = c("No", "Yes"))) 
+
