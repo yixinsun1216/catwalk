e1e976fe784ee1918e4827cc191d9b7fb90776c8
Yixin Sun <yixinsun1216@gmail.com>
2019-01-30 15:24:30 -0600

    move tests to test folder

--- tests/unit_test.R
+++ tests/unit_test.R
@@ -12,35 +12,41 @@ source(file.path(root, "R/latex_reader.R"))
 #===========
 # tests
 #===========
-# use freeny - Freeny's Revenue Data
+count_decimals <- function(no){
+	nchar(gsub("(.*\\.)|([0]*$)", "", as.character(no))) 
+}
 
 # want a function that takes in a parsnip object, extracts coefficients
 # and dimensions, and compares it to the latex output
 regtable_test <- function(df, r, f, ...){
 	test_fit <- fit_with(df, r, f, ...)[[1]] 
 
-	coef <- tidy(test_fit)
-	coef_names <- names(coef(test_fit))
+	coef_names <- 
+	  names(coef(test_fit)) 
 
 	latex_output <- 
 	  list(test_fit) %>%
 	  regtable(., est = coef_names, output_format = "latex") %>%
 	  latex_reader() %>%
 	  pluck("coef") %>%
-	  mutate_at(vars(-term, -type), funs(str_replace_all(., "\\(|\\)", "")))
-	  spread(type, V2)
+	  mutate_at(vars(-term, -type), funs(str_replace_all(., "\\(|\\)", ""))) %>%
+	  mutate_at(vars(-term, -type), as.numeric) %>%
+	  spread(type, V2) %>%
+	  select(term, estimate, std.error)
 
-	return(list(model_coef = coef, latex_coef = latex_output))
+	dec <- unique(count_decimals(latex_output$estimate))
+
+	coef <- 
+	  tidy(test_fit) %>%
+	  select(term, estimate, std.error) %>%
+	  mutate_if(is.numeric, funs(round(., digits = dec)))
+
+	eval(bquote(expect_equal(coef, latex_output)))
 }
 
+
+# use freeny - Freeny's Revenue Data
 test_that("testing single model, single independent variable", {
-	model <- regtable_test(freeny, felm, list(formula(y ~ lag.quarterly.revenue)))
-
-	test_that("one", {
-		expect_lte(8, 9)
-	})
-
-	test_that("two", {
-		expect_gte(8, 9)
-	})
+	regtable_test(freeny, felm, list(formula(y ~ lag.quarterly.revenue)))
+	regtable_test(freeny, lm, list(formula(y ~ lag.quarterly.revenue)))
 })
