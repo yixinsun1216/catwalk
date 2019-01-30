fdb9aac7d2e8b24415ce2036ee97ba859bc81387
Yixin Sun <yixinsun1216@gmail.com>
2019-01-30 16:21:25 -0600

    comparing coefficients when there are several models

--- tests/unit_test.R
+++ tests/unit_test.R
@@ -16,27 +16,39 @@ count_decimals <- function(no){
 	nchar(gsub("(.*\\.)|([0]*$)", "", as.character(no))) 
 }
 
+
+f <- list(formula(y ~ lag.quarterly.revenue + price.index))
+r <- list(lm, felm)
+
 # want a function that takes in a parsnip object, extracts coefficients
 # and dimensions, and compares it to the latex output
 regtable_test <- function(df, r, f, ...){
-	test_fit <- fit_with(df, r, f, ...)[[1]] 
+	test_fit <- 
+	  map2(r, f, function(x, y) fit_with(data = df, x, list(y))) %>%
+	  map(pluck(1))
 
-	coef_names <- names(coef(test_fit)) 
+	coef_names <- names(coef(test_fit[[1]])) 
 
 	latex_output <- 
-	  list(test_fit) %>%
+	  test_fit %>%
 	  regtable(., est = coef_names, output_format = "latex") %>%
 	  latex_reader() %>%
 	  pluck("coef") %>%
 	  mutate_at(vars(-term, -type), funs(str_replace_all(., "\\(|\\)", ""))) %>%
 	  mutate_at(vars(-term, -type), as.numeric) %>%
-	  spread(type, V2) %>%
-	  select(term, estimate, std.error)
+	  gather(key, value, -type, -term) %>%
+	  spread(type, value) %>%
+	  arrange(key) %>%
+	  select(-key)
 
 	dec <- unique(count_decimals(latex_output$estimate))
 
 	coef <- 
-	  tidy(test_fit) %>%
+	  test_fit %>% 
+	  tibble %>% 
+	  rename(model = 1) %>% 
+	  mutate(coef_info = map(model, tidy)) %>% 
+	  unnest(coef_info) %>%
 	  select(term, estimate, std.error) %>%
 	  mutate_if(is.numeric, funs(round(., digits = dec)))
 
@@ -46,6 +58,6 @@ regtable_test <- function(df, r, f, ...){
 
 # use freeny - Freeny's Revenue Data
 test_that("testing single model", {
-	regtable_test(freeny, felm, list(formula(y ~ lag.quarterly.revenue)))
-	regtable_test(freeny, lm, list(formula(y ~ lag.quarterly.revenue + price.index)))
+	regtable_test(freeny, list(felm), list(formula(y ~ lag.quarterly.revenue)))
+	regtable_test(freeny, list(felm, lm), list(formula(y ~ lag.quarterly.revenue + price.index)))
 })
