17a4af6e26d8f7fc216a45b247c850a2b8b88650
Michael Cahana <cahanamichael@gmail.com>
2019-05-16 13:00:53 -0500

    add more cleaning steps to read_latex()

--- R/read_latex.R
+++ R/read_latex.R
@@ -9,6 +9,9 @@
 #' @param keep_commas A Boolean specifying whether to keep commas in the table.
 #' @param keep_parens A Boolean specifying whether to keep parentheses in the 
 #' table.
+#' @param keep_empties A Boolean specifying whether to keep empty 
+#' variable names in the table (as opposed to backward-filling or 
+#' forward-filling them). 
 #' @return A tibble. 
 #' @examples
 #' library(lfe)
@@ -55,58 +58,140 @@ NULL
 # read latex
 #===========
 
-#' 
+library(zoo)
+library(tidyverse)
+
+empty_as_na <- function(x){
+    if("factor" %in% class(x)) x <- as.character(x) 
+    ifelse(as.character(x)!="", x, NA)
+}
+
 #' @rdname read_latex
-read_latex <- function(latex_file, keep_sig_stars = FALSE, keep_commas = TRUE, 
-  keep_parens = TRUE){
+read_latex <- function(latex_file, keep_sig_stars = TRUE, keep_commas = TRUE, 
+  keep_parens = TRUE, keep_empties = FALSE){
+
   latex <- readLines(latex_file)
 
+  # extract multicolumn header names (if they exist)
+  multicolumn_header <- 
+    latex[str_detect(latex, 'multicolumn')] %>% 
+    str_split("&") %>%
+    map_df(function(x) as_tibble(t(x))) %>% 
+    mutate_all(funs(str_replace_all(., 
+      '\\\\[a-zA-Z]+\\{.*?\\}\\{.*?\\}', ''))) %>% 
+    mutate_all(funs(str_replace_all(., '\\{', ''))) %>% 
+    mutate_all(funs(str_replace_all(., '\\}', ''))) %>% 
+    mutate_all(funs(str_replace_all(., '\\\\', ''))) %>% 
+    mutate_all(funs(str_replace_all(., 'textit', ''))) %>% 
+    mutate_all(trimws) %>% 
+    .[1,] %>% 
+    as.character() %>% 
+    .[.!='']
+
+  # split latex by ampersand
   latex <- 
     latex %>%
-      str_split("&") %>%
-      map_df(function(x) as_tibble(t(x))) %>%
+      str_split("&")
+
+  # put latex into tibble format, suppress the .name_repair argument warning.
+  # to suppress this warning the pipeline unfortunately has to be broken out of
+  latex <- suppressWarnings(map_df(latex, function(x) as_tibble(t(x))))
+
+  # clean up names and special characters
+  latex <- 
+    latex  %>%
       rename(stats_name = 1) %>% 
       na.omit() %>%
       mutate_all(funs(str_replace_all(., "\\\\", ""))) %>%
       mutate_all(funs(if_else(. == "", NA_character_, .))) %>%
       mutate_all(funs(str_replace_all(., 'phantom\\{.*?\\}', ''))) %>% 
-      mutate_all(funs(str_replace_all(., '[a-zA-Z]+\\{.*?\\}\\{.*?\\}', ''))) %>%
+      mutate_all(funs(str_replace_all(., 
+        '[a-zA-Z]+\\{.*?\\}\\{.*?\\}', ''))) %>%
       mutate_all(funs(str_replace_all(., '[{}]', ''))) %>%
       mutate_all(funs(str_replace_all(., '\\$', ''))) %>% 
-      mutate_all(funs(str_replace_all(., 'raggedrightarraybackslash', ''))) %>% 
+      mutate_all(funs(str_replace_all(., 
+        'raggedrightarraybackslash', ''))) %>% 
+      mutate_all(funs(str_replace_all(., 'textit', ''))) %>% 
+      mutate_all(funs(str_replace_all(., 'hline', ''))) %>% 
       mutate_all(trimws) %>% 
-      mutate(stats_name = ifelse(row_number()==1 & stats_name=='', 
-        'stats_name', stats_name)) %>% 
-      mutate(stats_name = ifelse(stats_name=='', NA, stats_name)) %>% 
-      setNames(as.character(.[1,])) %>% 
-      .[-1,]
+      mutate_all(funs(str_replace_all(., '\\[-1.8ex\\]', '')))
 
-  empties <- 
-    latex %>% 
-    mutate(row_num = row_number()) %>% 
-    filter(is.na(.[,1])) %>% 
-    pull(row_num)
-
-  if (sum(empties %% 2)==0) { # if even
-    fromLast = FALSE
-  } else {
-    fromLast = TRUE
+  # remove the redundant stargazer multicolumn header
+  if (sum(str_detect(latex[1,], 'Dependent variable:'))>0) {
+    latex <- latex[-1,]
   }
 
+  # set first column name to stats_name if it doesn't exist otherwise
   latex <- 
-    latex %>% 
-    na.locf(fromLast = fromLast)
+    latex %>%
+    mutate(stats_name = ifelse(row_number()==1 & stats_name=='', 
+      'stats_name', stats_name)) %>% 
+    mutate_all(funs(empty_as_na)) %>% 
+    setNames(as.character(.[1,])) %>% 
+    .[-1,]
 
+  # group together multiple headers, if the first set of headers are needed to 
+  # distinguish between duplicate names in the second set of headers  
+  for (name in colnames(latex)) {
+    indices <- which(name == colnames(latex)) 
+    if (length(indices)>1) {
+      if (length(multicolumn_header)>0) {
+        for (i in 1:length(multicolumn_header)) {
+          colnames(latex)[indices[i]] = paste(colnames(latex)[indices[i]], 
+            multicolumn_header[i], sep = ' - ')
+        }
+      } else {
+        for (i in 1:length(indices)) {
+          colnames(latex)[indices[i]] = paste(colnames(latex)[indices[i]], 
+            i, sep = ' - ')
+        }
+      }
+    }
+  }
+
+  # remove entirely null rows 
+  latex <- 
+    latex %>%   
+    filter_all(any_vars(!is.na(.)))
+
+  # optionally rename empty variable names to those immediately 
+  # preceding or proceeding them (preceding if the empties are in an odd order)
+  if (!keep_empties) {
+    empties <- 
+      latex %>% 
+      mutate(row_num = row_number()) %>% 
+      filter(is.na(.[,1])) %>% 
+      pull(row_num)
+
+    if (empties[1]>2) {
+      empties <- empties - (empties[1] - 2)
+    }
+
+    if (sum(empties %% 2)==0) { # if even
+      fromLast = FALSE
+    } else {
+      fromLast = TRUE
+    }
+
+    latex[1] <- 
+      latex[1] %>% 
+      na.locf(fromLast = fromLast)
+  }
+
+  # optionally remove significance stars
   if (!keep_sig_stars) {
     latex <- 
       latex %>% 
-      mutate_all(funs(str_replace_all(., '\\^[*].*$', '')))
+      mutate_all(funs(str_replace_all(., '\\^[*].*$', ''))) %>% 
+      mutate_all(funs(str_replace_all(., 'sym|\\*', '')))
   }
+  # optionally remove commas
   if (!keep_commas) {
     latex <- 
       latex %>% 
       mutate_all(funs(str_replace_all(., ',', '')))
   } 
+  # optionally remove parentheses
   if (!keep_parens) {
     latex <- 
       latex %>% 
@@ -117,3 +202,28 @@ read_latex <- function(latex_file, keep_sig_stars = FALSE, keep_commas = TRUE,
   return (latex)
 }
 
+extract_column <- function(col, stats) {
+  col_estimate <- 
+      stats %>% 
+      pull(col) %>% 
+      str_replace('\\(', '') %>% 
+      str_replace('\\)', '') %>% 
+      str_replace(',', '') %>% 
+      str_replace_all(., '\\^[*].*$', '') %>% 
+      str_replace_all(., '\\*', '') %>% 
+      as.double()
+  return (col_estimate)
+}
+
+pull_numbers <- function(df, variable, cols)  {
+  stats <- 
+    df %>% 
+    filter_at(1, all_vars(. == variable)) %>% 
+    .[-1]
+
+  if (is.null(cols)) {cols <- seq(1, dim(stats)[2])}
+  
+  estimates <- map(cols, extract_column, stats)
+
+  return (estimates)
+}
